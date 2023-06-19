f0ace92609500e4a7e8ac42cd6c5531dc9b1f82c
Bikiway <mariana.95@outlook.pt>
2023-06-19 18:00:06 +0100

    Just Started 1º class

---
+++ AirFiel-Mariana_Oliveira/Data/Entities/Airplanes.cs
@@ -0,0 +1,17 @@
+﻿namespace AirFiel_Mariana_Oliveira.Data.Entities
+{
+    public class Airplanes
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string classes { get; set; }
+
+
+
+
+
+        public string Description { get; set; }
+    }
+}
